70d4761580df8a2d6367aaecadc89cb83fcef231
Raj Modi <modiraj340@gmail.com>
2025-10-22 16:44:02 +0530

    Update cookie settings for token handling

--- backened/controllers/auth.js
+++ backened/controllers/auth.js
@@ -21,8 +21,8 @@ const token= await genToken(user._id);
 res.cookie("token",token,{
  httpOnly:true,
        maxAge:7*24*60*60*1000,
-       sameSite:"strict",
-       secure:false
+       sameSite:"None",
+       secure:true
 })
 return res.status(201).json({
     message:"User created successfully",
@@ -47,8 +47,8 @@ export const Login=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:false
+            sameSite:"None",
+       secure:true
         })
         return res.status(200).json({
             message:"Login successful",
